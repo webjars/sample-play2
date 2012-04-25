e2939ce5d92211fa3111a51cb12327997d30e29b
James Ward <james@jamesward.org>
2012-04-25 07:33:29 -0600

    init

---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "play2_webjars_demo"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    val appDependencies = Seq(
+      "com.github.twitter" % "bootstrap" % "2.0.2"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      resolvers += "webjars" at "http://webjars.github.com/m2"
+    )
+
+}
