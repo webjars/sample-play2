ed09af326966a4cd76e32b5e27e5c5423e8421f7
James Ward <james@jamesward.org>
2012-10-11 10:52:14 -0600

    update to bootstrap 2.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.github.twitter" % "bootstrap" % "2.0.3"
+      "org.webjars" % "bootstrap" % "2.1.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
