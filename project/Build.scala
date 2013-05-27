b19ccab12434daf2e34ae201684c7382793e9386
James Ward <james@jamesward.org>
2013-05-27 10:27:22 -0600

    test latest

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.webjars" % "webjars-play" % "2.1.0-1",
+    "org.webjars" %% "webjars-play" % "2.1.0-2-SNAPSHOT",
     "org.webjars" % "bootstrap" % "2.1.1"
   )
 
